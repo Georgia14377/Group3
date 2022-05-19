4255aff43e12c3fb8f85eb66b759bf52879bbdca
105328501 <105328501@L815WD220017.edu.cbe.ab.ca>
2022-05-19 14:46:43 -0600

    ah

--- Group3Project/src/Group3Project.java
+++ Group3Project/src/Group3Project.java
@@ -1,9 +1,31 @@
 
 public class Group3Project {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	/*
+	Program: Group3Project.java          Date: May 19th 2022
+	Author: Georgia Mollet, ---, ----, ----
+	School: CHHS
+	Course: Computer Science 10
+*/
+	public static void main(String[] args) 
+	
+	{
+		
+		
+			Scanner userinput = new Scanner(System.in);
+					
+					System.out.println("Please enter any three digits:"); 
+				
+					int dig = userinput.nextInt();
+					
+					int dig1 = dig / 100;
+					
+							
+							
+					int dig2 = (dig % 100) / 10;
+					
+					
+					int dig3 = dig % 10;
 	}
 
-}
+
